2d859d2d5abd14a7b87fda4a81b8a2cf1e7e9819
leileitun <143359786+leileitun@users.noreply.github.com>
2023-10-12 18:51:28 -0400

    new hw

---
+++ solution-hw6/js/forcart.js
@@ -0,0 +1,114 @@
+//source: github followed the pui lab note end to build and remove cart items 
+class Roll {
+  constructor(rollType, rollGlazing, packSize, basePrice) {
+    this.type = rollType;
+    this.glazing =  rollGlazing;
+    this.size = packSize;
+    this.basePrice = basePrice;
+  }
+}
+
+// const cart = []; //empty array to store my cart items
+
+let glazingDictionary = {
+  original: {name: "Keep original", value: 0.00},
+  sugar: {name: "Sugar milk", value: 0.00},
+  vanilla: {name: "Vanilla milk", value: 0.50},
+  chocolate: {name: "Double chocolate", value: 1.50}
+};
+
+let packDictionary = [
+  {name: "1", value: 1},
+  {name: "3", value: 3},
+  {name: "6", value: 5},
+  {name: "12", value: 10}
+];
+
+function calculatePrice(roll){ 
+  let glazingPrice = 0;
+  for (let element in glazingDictionary){
+    if (roll.glazing === glazingDictionary[element].name){
+      glazingPrice = glazingDictionary[element].value;
+      break;
+    }
+  }
+  let packSizeSize = 1; 
+  for (let element in packDictionary){
+    if (roll.size === packDictionary[element].name){
+      packSizeSize = packDictionary[element].value; 
+      break;
+    }
+  }
+  let totalPrice = (roll.basePrice + glazingPrice)*packSizeSize;
+  return totalPrice;
+}
+
+
+function displayCart(roll){ 
+  const cartContainer = document.querySelector('.checkout');
+  const cartItem = document.querySelector('#cart-template').content.cloneNode(true);  //clone new row and returns it 
+  roll.element = cartItem.querySelector('.newcontainer');
+
+  cartItem.querySelector('#cartpic').src = `../assets/products/${roll.type.toLowerCase()}` +`-cinnamon-roll.jpg`;
+  cartItem.querySelector('#name').textContent = roll.type + " Cinnamon Roll";
+  cartItem.querySelector('#glaze').textContent = "Glazing: " + roll.glazing;
+  cartItem.querySelector('#packsize').textContent = "Pack Size: " + roll.size;
+  cartItem.querySelector('.pricepoint').textContent = "$ " + calculatePrice(roll).toFixed(2);
+    
+  const removelink = cartItem.querySelector('.removal'); 
+  removelink.addEventListener('click', () => {
+    deleteItem(roll);
+    });
+
+    cartContainer.appendChild(cartItem);
+}
+
+//need a function to calcualte the total price and display the total price
+
+function calculateTotalPrice(cart) {
+  let totalPrice = 0;
+  for (let roll of cart) {
+    totalPrice = totalPrice + calculatePrice(roll);
+  }
+  return totalPrice.toFixed(2);
+}
+
+function displayTotalPrice() {
+  const totalpriceElement = document.querySelector('.totalprice');
+  const total = calculateTotalPrice(cart);
+  totalpriceElement.textContent = "$ " + total;
+}
+
+//sourced mozilla developer to see syntax for array.prototype.splice
+function deleteItem(roll){
+  const index = cart.indexOf(roll); 
+  if (index != -1){
+    cart.splice(index, 1);
+    displayTotalPrice();
+}
+roll.element.remove();
+}
+
+for (let roll of cart){
+  displayCart(roll);
+}
+
+displayTotalPrice();
+
+
+function retrieveFromLocalStorage(){ 
+  const cartString = localStorage.getItem('storedCart');
+  const cartArray = JSON.parse(cartString);
+  for (const rollData of cartArray){
+    const rollie = new Roll(rollData.type, rollData.glazing, rollData.size, rollData.basePrice);
+    cart.push(rollie); 
+  }
+  for (const roll of cart){
+    displayCart(roll);
+    displayTotalPrice();
+  }
+}
+
+if (localStorage.getItem('storedCart') != null){
+  retrieveFromLocalStorage();
+}
---
+++ solution-hw6/js/main.js
@@ -0,0 +1,129 @@
+// create objects within with key-value pairs with name and value 
+class Roll {
+  constructor(rollType, rollGlazing, packSize, basePrice) {
+    this.type = rollType;
+    this.glazing =  rollGlazing;
+    this.size = packSize;
+    this.basePrice = basePrice;
+  }
+}
+
+let glazingDictionary = {
+  original: {name: "Keep original", value: 0.00},
+  sugar: {name: "Sugar milk", value: 0.00},
+  vanilla: {name: "Vanilla milk", value: 0.50},
+  chocolate: {name: "Double chocolate", value: 1.50}
+};
+
+let packDictionary = [
+  {name: "1", value: 1},
+  {name: "3", value: 3},
+  {name: "6", value: 5},
+  {name: "12", value: 10}
+];
+
+//three interactions: glaze, size, prices
+//retrieve HTML elements so we can interact with them
+let glazingElement = document.querySelector('#glazing-select'); 
+let packElement = document.querySelector('#pack-select');
+let price = document.querySelector('#inlineprice'); 
+
+//source summer course slides for for loop set up 
+for (let i in glazingDictionary){ //iterate over each object
+  let glazeoption = document.createElement("option"); //creates option element and assigns it to glazeooption
+  glazingElement.add(glazeoption); //adding elements one by one
+  glazeoption.innerHTML = glazingDictionary[i].name; //glazingDictionary[original] = "keep original"
+  // glazeoption.value = glazingDictionary[i].value; //glazingDictionary[original] = 0.00
+}
+
+for (let i in packDictionary){ 
+  let packoption = document.createElement("option");
+  packElement.add(packoption);
+  packoption.innerHTML = packDictionary[i].name;
+  //packoption.value = packDictionary[i].value; 
+}
+
+const queryString = window.location.search; 
+const params = new URLSearchParams(queryString); //parses string to access values
+const rollType = params.get('roll');//gets value of roll from URL
+
+const rollTitle = document.querySelector('#imagetitle'); 
+rollTitle.innerText = rollType + ' Cinnamon Roll'; 
+
+const rollImage = document.querySelector('#cartitem');
+rollImage.src = '../assets/products/' + rolls[rollType].imageFile; 
+
+//take the HTML elements and we want them to react on change (event type) and when changed, activate functions
+glazingElement.addEventListener("change", glazingChange);
+packElement.addEventListener("change", packChange);
+
+let newbase = rolls[rollType]['basePrice'];
+let glazePrice = 0; 
+let totalPrice = 0; 
+let packSize = 1;
+
+displayPrice(updatePrice());
+
+//when change event happens, we want to update glazePrice to whatever the value is gonna be
+//source for parseFloat: stackoverflow when looking up NaN error
+function glazingChange() {
+  for (const element in glazingDictionary){
+    if (glazingElement.value === glazingDictionary[element].name) {//not variable value but the actual dropdown value
+      glazePrice = glazingDictionary[element].value; 
+    }
+  }
+  // console.log(glazingElement.value);
+  glazePrice = parseFloat(glazePrice); 
+  // console.log(glazePrice);
+  displayPrice(updatePrice()); 
+}
+
+function packChange(){
+  let packSize = packElement.value; 
+  //console.log(packSize);
+  displayPrice(updatePrice()); 
+}
+
+//source: stackoverflow to look up for loop to access dictionary values using element when showing error for packsize
+function updatePrice(){
+  let packSizeValue;
+  for (const element in packDictionary){
+    if (packDictionary[element].name === packElement.value) {
+      packSizeValue = packDictionary[element].value;
+      //console.log(element);
+      //console.log(packSizeValue);
+    }
+  }
+  packSize = packSizeValue;
+  totalPrice = (newbase + glazePrice)*packSize; 
+  // console.log(totalPrice);
+  return totalPrice; 
+}
+
+updatePrice();
+
+function displayPrice(totalPrice){
+  price.textContent = "$ " + totalPrice.toFixed(2);
+}
+
+updatePrice();
+
+//source: summer course lecture on array push and addEventListener
+const cartButton = document.querySelector('#cartbutton');
+cartButton.addEventListener("click", function(){
+  console.log('cart before', cart)
+  const glazing = glazingElement.value; 
+  const pack = packElement.value; 
+  const newRoll = new Roll(rollType, glazing, pack, newbase);
+  cart.push(newRoll);
+  console.log('cart after', cart)
+  saveToLocalStorage();
+});
+
+//cart has to come from local storage//
+
+function saveToLocalStorage(){ 
+  const cartString = JSON.stringify(cart);
+  localStorage.setItem('storedCart',cartString); 
+}
+
